2a1f0ce6e17291f9465e66458b7588dfedd87fcb
Alexandre LOUBEAU <loubeau.alexandre3@gmail.com>
2022-12-08 18:50:31 +0100

    Ajout d'un constructeur dans le fichier din.go

---
+++ gi.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+
+
+type Valentin struct {
+	a string
+	b int
+	c bool
+}
+
+func main() {
+	mesCouillesSurUnTatami := Valentin {
+		a: "Valentin",
+		b: 24,
+		c: true,
+	}
+
+	lePlusGrandDesMagiciens := Valentin {
+		a:"Je suis le plus grand des Magiciens",
+		b: 190,
+		c: true,
+	}
+
+	bigConpyuteur := Valentin {
+		a: "Où est le BIG CONPYUTEUR ?",
+		b: 50,
+		c:false,
+	}
+
+	jeCPoCommentNommerLaVariable := Valentin {"Alexis", 28,false}
+
+
+	fmt.Println(mesCouillesSurUnTatami.a)
+	fmt.Println(lePlusGrandDesMagiciens.b)
+	fmt.Println(bigConpyuteur)
+	fmt.Println(jeCPoCommentNommerLaVariable.a)
+
+	myContact := newContact("Alex", 30)
+	fmt.Println("Ma fonciton Contact : ", myContact)
+}
--- le.go
+++ le.go
@@ -1,20 +1,20 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func update(pointerOfNumber *int, value int) {
-	*pointerOfNumber = value
-}
+// func update(pointerOfNumber *int, value int) {
+// 	*pointerOfNumber = value
+// }
 
-func main() {
-	number := 10
-	fmt.Println(number)
-	fmt.Println("Adresse mémoire de number : ", &number)
+// func main() {
+// 	number := 10
+// 	fmt.Println(number)
+// 	fmt.Println("Adresse mémoire de number : ", &number)
 
-	myPointer := &number
-	fmt.Println(myPointer)
-	fmt.Printf("La valeur de l'adresse mémoire %v est %d.\n", myPointer, *myPointer)
+// 	myPointer := &number
+// 	fmt.Println(myPointer)
+// 	fmt.Printf("La valeur de l'adresse mémoire %v est %d.\n", myPointer, *myPointer)
 
-	update(myPointer, 20)
-	fmt.Printf("La valeur de l'adresse mémoire %v a changé -> %d", myPointer, number)
-}
+// 	update(myPointer, 20)
+// 	fmt.Printf("La valeur de l'adresse mémoire %v a changé -> %d", myPointer, number)
+// }
