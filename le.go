0788de150bab3f0476914aa240ff87795b5741d3
Alexandre LOUBEAU <loubeau.alexandre3@gmail.com>
2022-12-08 16:08:29 +0100

    Création d'un POINTER

--- le.go
+++ le.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println()
-
-	valentinGo(13,14)
+func update(pointerOfNumber *int, value int) {
+	*pointerOfNumber = value
 }
 
+func main() {
+	number := 10
+	fmt.Println(number)
+	fmt.Println("Adresse mémoire de number : ", &number)
 
-func valentinGo(x, y int) {
-	fmt.Printf("dis bonjour !")
+	myPointer := &number
+	fmt.Println(myPointer)
+	fmt.Printf("La valeur de l'adresse mémoire %v est %d.\n", myPointer, *myPointer)
+
+	update(myPointer, 20)
+	fmt.Printf("La valeur de l'adresse mémoire %v a changé -> %d", myPointer, number)
 }
